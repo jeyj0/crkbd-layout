c18adaf64baec1d70b1f5c094441e1c67235a4c2
Jannis Jorre <jannis@jorre.dev>
2019-08-15 14:31:51 +0200

    (feat): better accessibility of arrow keys and different access to vim
    - Reasons for change:
    	- accidental switching into vim mode
    	- hard access to arrow keys
    - New behavior: vim layer (with arrow keys) is active while holding down
    the previous UP_DOWN key, and stays active if the key is tapped three
    times in a row

--- keymap.c
+++ keymap.c
@@ -31,7 +31,6 @@ enum custom_kcs {
 	VIM_INSERT,
 	ESC,
 	VIM_ENT,
-	UP_DOWN,
 	CC_VNT
 };
 
@@ -93,28 +92,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				tap_code(KC_HOME);
 			}
 			break;
-		case UP_DOWN: {
-			static uint16_t kc;
-
-			if (record->event.pressed) {
-				bool isShifted = get_mods() & MOD_MASK_SHIFT;
-
-				if (isShifted) {
-					del_mods(MOD_MASK_SHIFT);
-					kc = KC_DOWN;
-				} else {
-					kc = KC_UP;
-				}
-
-				register_code(kc);
-
-				if (isShifted) {
-					register_code(KC_LSFT);
-				}
-			} else {
-				unregister_code(kc);
-			}
-		}
 	}
 	return true;
 }
@@ -123,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT(
 			KC_TAB,    KC_Q,     KC_W,     KC_E,     LT(_RESET, KC_R), KC_T,             KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
 			ESC,       KC_A,     KC_S,     KC_D,     KC_F,             KC_G,             KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-			UP_DOWN,   KC_Z,     KC_X,     KC_C,     KC_V,             KC_B,             KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLASH,
+			TT(_VIM),  KC_Z,     KC_X,     KC_C,     KC_V,             KC_B,             KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLASH,
 			                               KC_LCTL,  CODING,           ALT,              KC_ENT,   SFT_T(KC_SPC),   FUNCTIONS
 			),
 
@@ -137,14 +114,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_VIM] = LAYOUT(
 			XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, 		XXXXXXXX, XXXXXXXX, VIM_INSERT, XXXXXXXX, XXXXXXXX, KC_LEFT,
 			KC_ESC,   XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, 		KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT, XXXXXXXX, XXXXXXXX,
-			BASE,     XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, 		XXXXXXXX, XXXXXXXX, XXXXXXXX,   XXXXXXXX, XXXXXXXX, XXXXXXXX,
+			BASE,     XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, 		XXXXXXXX, XXXXXXXX, XXXXXXXX,   XXXXXXXX, XXXXXXXX, VIM,
 										  ________, ________, ________, 		VIM_ENT,  XXXXXXXX, ________
 			),
 
 	[_CODING] = LAYOUT(
 			KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,             KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
 			________, XXXXXXXX, XXXXXXXX, KC_MINS,  KC_PLUS,  KC_EQL,           XXXXXXXX, KC_LBRC,  KC_RBRC,  XXXXXXXX, XXXXXXXX, XXXXXXXX,
-			________, XXXXXXXX, XXXXXXXX, VIM,      XXXXXXXX, XXXXXXXX,         XXXXXXXX, KC_LPRN,  KC_RPRN,  XXXXXXXX, XXXXXXXX, ________,
+			________, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX,         XXXXXXXX, KC_LPRN,  KC_RPRN,  XXXXXXXX, XXXXXXXX, ________,
 			                              ________, ________, ________,         ________, ________, ________
 			),
 
