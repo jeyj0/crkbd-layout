72f716302bedb901592c54f9e9146e5e4acd2e9f
Jannis Jorre <jeyj0.plus@gmail.com>
2019-11-13 18:53:50 +0100

    (feat): enter now is SUPER when held

--- keymap.c
+++ keymap.c
@@ -259,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TAB,    KC_Q,     KC_W,     KC_E,     LT(_RESET, KC_R), KC_T,             KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
 			ESC,       KC_A,     KC_S,     KC_D,     KC_F,             KC_G,             KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
 			TT_VIM,    KC_Z,     KC_X,     KC_C,     KC_V,             KC_B,             KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLASH,
-			                               KC_LCTL,  CODING,           ALT,              KC_ENT,   SFT_T(KC_SPC),   FUNCTIONS
+			                               KC_LCTL,  CODING,           ALT,              LGUI_T(KC_ENT),   SFT_T(KC_SPC),   FUNCTIONS
 			),
 
 	[_GAMING] = LAYOUT(
