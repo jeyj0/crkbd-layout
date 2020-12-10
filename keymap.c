5d552e59957637838e5471f0bba38543a4390398
Jannis Jorre <jannis@jorre.dev>
2020-12-10 19:36:56 +0100

    nice utility layers on the right hand

--- keymap.c
+++ keymap.c
@@ -3,13 +3,14 @@
 enum crkbd_layers {
     _BASE,
     _COLEMAK_DH,
-    _QUICK_ACCESS_RIGHT,
+    _RIGHT_1,
+    _RIGHT_2,
+    _RIGHT_3,
     _GAMING,
     _GAMING_NUMPAD,
     _CODING,
     _ALT,
     _FUNCTIONS,
-    _RIGHT_SPECIAL,
     _RESET
 };
 
@@ -46,29 +47,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
             KC_BSPC,      KC_P,         KC_O,         KC_I,         KC_U,         KC_Y,               KC_T,         LT(_RESET, KC_R), KC_E,     KC_W,         KC_Q,         KC_TAB,
-            KC_QUOT,      KC_SCLN,      KC_L,         KC_K,         KC_J,         KC_H,               KC_G,         LT(_QUICK_ACCESS_RIGHT, KC_F), LT(_RIGHT_SPECIAL, KC_D),KC_S,KC_A,         KC_ESC,
+            KC_QUOT,      KC_SCLN,      KC_L,         KC_K,         KC_J,         KC_H,               KC_G,         LT(_RIGHT_1,KC_F),LT(_RIGHT_2,KC_D),LT(_RIGHT_3,KC_S),KC_A,KC_ESC,
             KC_BSLASH,    KC_SLSH,      KC_DOT,       KC_COMM,      KC_M,         KC_N,               KC_B,         KC_V,         KC_C,         KC_X,         KC_Z,         DF(_COLEMAK_DH),
                                                       FUNCTIONS,    SFT_T(KC_SPC),LGUI_T(KC_ENT),     ALT,          CODING,       KC_LCTL
             ),
 
     [_COLEMAK_DH] = LAYOUT(
-            KC_BSPC,      KC_SCLN,      KC_Y,         KC_U,         KC_L,         KC_J,               KC_B,         KC_P,         KC_F,         KC_W,         KC_Q,         KC_TAB,
-            KC_QUOT,      KC_O,         KC_I,         KC_E,         KC_N,         KC_K,               KC_G,         LT(_QUICK_ACCESS_RIGHT, KC_T), LT(_RIGHT_SPECIAL, KC_S),KC_R,         KC_A,         KC_ESC,
+            KC_BSPC,      KC_SCLN,      KC_Y,         KC_U,         KC_L,         KC_J,               KC_B,         LT(_RESET, KC_P), KC_F,         KC_W,         KC_Q,         KC_TAB,
+            KC_QUOT,      KC_O,         KC_I,         KC_E,         KC_N,         KC_K,               KC_G,         LT(_RIGHT_1,KC_T),LT(_RIGHT_2,KC_S),LT(_RIGHT_3,KC_R),KC_A,KC_ESC,
             KC_BSLASH,    KC_SLSH,      KC_DOT,       KC_COMM,      KC_H,         KC_M,               KC_V,         KC_D,         KC_C,         KC_X,         KC_Z,         DF(_BASE),
                                                       FUNCTIONS,    SFT_T(KC_SPC),LGUI_T(KC_ENT),     ALT,          CODING,       KC_LCTL
             ),
 
-    [_QUICK_ACCESS_RIGHT] = LAYOUT(
+    [_RIGHT_1] = LAYOUT(
             ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
             ________,     ________,     KC_RIGHT,     KC_UP,        KC_DOWN,      KC_LEFT,            ________,     ________,     ________,     ________,     ________,     ________,
             ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
                                                       ________,     ________,     ________,           ________,     ________,     ________
             ),
 
-    [_RIGHT_SPECIAL] = LAYOUT(
-            ________,     ________,     ________,     DM_PLY2,      DM_REC2,      ________,           ________,     ________,     ________,     ________,     ________,     ________,
-            ________,     ________,     ________,     DM_PLY1,      DM_REC1,      ________,           ________,     ________,     ________,     ________,     ________,     ________,
-            ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
+    [_RIGHT_2] = LAYOUT(
+            DM_REC2,      DM_REC1,      KC_9,         KC_8,         KC_7,         ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            DM_PLY2,      DM_PLY1,      KC_6,         KC_5,         KC_4,         ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            ________,     ________,     KC_3,         KC_2,         KC_1,         ________,           ________,     ________,     ________,     ________,     ________,     ________,
+                                                      ________,     KC_0,         ________,           ________,     ________,     ________
+            ),
+
+    [_RIGHT_3] = LAYOUT(
+            ________,     ________,     ________,     S(KC_RBRC),   S(KC_LBRC),   ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            ________,     ________,     ________,     KC_RBRC,      KC_LBRC,      ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            ________,     ________,     ________,     KC_RPRN,      KC_LPRN,      ________,           ________,     ________,     ________,     ________,     ________,     ________,
                                                       ________,     ________,     ________,           ________,     ________,     ________
             ),
 
