9a216dbe529a58721060059bc43bbc4c07731d1f
Jannis Jorre <jannis@jorre.dev>
2020-12-10 19:07:25 +0100

    add colemak layout

--- keymap.c
+++ keymap.c
@@ -2,6 +2,7 @@
 
 enum crkbd_layers {
     _BASE,
+    _COLEMAK_DH,
     _QUICK_ACCESS_RIGHT,
     _GAMING,
     _GAMING_NUMPAD,
@@ -22,13 +23,11 @@ enum crkbd_layers {
 #define ________ KC_TRNS
 #define XXXXXXXX KC_NO
 
-// move workspaces down / up; assuming alt is already pressed
-#define WORKDOWN LCTL( KC_DOWN )
-#define WORKUP   LCTL( KC_UP )
-
 enum custom_kcs {
     BASE = SAFE_RANGE,
 };
+
+int isColemak = false;
 
 // custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -47,8 +46,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
             KC_BSPC,      KC_P,         KC_O,         KC_I,         KC_U,         KC_Y,               KC_T,         LT(_RESET, KC_R), KC_E,     KC_W,         KC_Q,         KC_TAB,
-            KC_QUOT,      KC_SCLN,      KC_L,         KC_K,         KC_J,         KC_H,               KC_G,         LT(_QUICK_ACCESS_RIGHT, KC_F), LT(_RIGHT_SPECIAL, KC_D), KC_S,KC_A,         KC_ESC,
-            KC_BSLASH,    KC_SLSH,      KC_DOT,       KC_COMM,      KC_M,         KC_N,               KC_B,         KC_V,         KC_C,         KC_X,         KC_Z,         KC_LSFT,
+            KC_QUOT,      KC_SCLN,      KC_L,         KC_K,         KC_J,         KC_H,               KC_G,         LT(_QUICK_ACCESS_RIGHT, KC_F), LT(_RIGHT_SPECIAL, KC_D),KC_S,KC_A,         KC_ESC,
+            KC_BSLASH,    KC_SLSH,      KC_DOT,       KC_COMM,      KC_M,         KC_N,               KC_B,         KC_V,         KC_C,         KC_X,         KC_Z,         DF(_COLEMAK_DH),
+                                                      FUNCTIONS,    SFT_T(KC_SPC),LGUI_T(KC_ENT),     ALT,          CODING,       KC_LCTL
+            ),
+
+    [_COLEMAK_DH] = LAYOUT(
+            KC_BSPC,      KC_SCLN,      KC_Y,         KC_U,         KC_L,         KC_J,               KC_B,         KC_P,         KC_F,         KC_W,         KC_Q,         KC_TAB,
+            KC_QUOT,      KC_O,         KC_I,         KC_E,         KC_N,         KC_K,               KC_G,         LT(_QUICK_ACCESS_RIGHT, KC_T), LT(_RIGHT_SPECIAL, KC_S),KC_R,         KC_A,         KC_ESC,
+            KC_BSLASH,    KC_SLSH,      KC_DOT,       KC_COMM,      KC_H,         KC_M,               KC_V,         KC_D,         KC_C,         KC_X,         KC_Z,         DF(_BASE),
                                                       FUNCTIONS,    SFT_T(KC_SPC),LGUI_T(KC_ENT),     ALT,          CODING,       KC_LCTL
             ),
 
@@ -88,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
 
     [_ALT] = LAYOUT(
-            ________,     ________,     ________,     ________,     ________,     ________,           ________,     KC_F4,        ________,     ________,     ________,     ________,
             ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
-            ________,     ________,     ________,     WORKUP,       WORKDOWN,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
+            ________,     ________,     ________,     ________,     ________,     ________,           ________,     ________,     ________,     ________,     ________,     ________,
                                                       CODING,       ________,     ________,           ________,     ________,     ________
             ),
 
